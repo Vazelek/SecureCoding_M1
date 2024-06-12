0f857aa878aacff3dcd0673a88cefdd0ea6076a8
Vazelek <valentinherve60@gmail.com>
2024-06-12 15:08:32 +0200

    Initial commit

---
+++ src/main/java/hr/ticketmaster/finder/ai/ticketmasterfinderai/controller/rest/TicketsRestController.java
@@ -0,0 +1,36 @@
+package hr.ticketmaster.finder.ai.ticketmasterfinderai.controller.rest;
+
+import hr.ticketmaster.finder.ai.ticketmasterfinderai.model.Ticket;
+import hr.ticketmaster.finder.ai.ticketmasterfinderai.model.TicketTypeEnum;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("rest")
+public class TicketsRestController {
+
+    private static List<Ticket> ticketList;
+
+
+    @GetMapping("/tickets")
+    public List<Ticket> getTicketList() {
+        return ticketList;
+    }
+
+    @GetMapping("/ticket/{id}")
+    public Ticket getTicketById(@PathVariable Integer id) {
+        return ticketList.stream()
+                .filter(t -> t.getId().compareTo(id) == 0)
+                .toList().getFirst();
+    }
+
+    @PostMapping("/ticket")
+    public void createNewTicket(@RequestBody Ticket ticket) {
+        ticketList.add(ticket);
+    }
+
+}
